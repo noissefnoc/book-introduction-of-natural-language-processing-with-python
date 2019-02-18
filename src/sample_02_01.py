c6a1e26b0b804f20babe530b5cbf1b21558e3df0
noissefnoc <noissefnoc@gmail.com>
2019-02-19 01:40:21 +0900

    get "Japanese" wikipedia page written by Japanese.

---
+++ src/sample_02_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import urllib.request
+
+if __name__ == '__main__':
+    # "Japanese" wikipedia page URL written by Japanese
+    url = 'https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC'
+
+    with urllib.request.urlopen(url) as res:
+        byte = res.read()
+
+        # convert character encording
+        html = byte.decode('utf-8')
+        print(html)
